b219dbba8e3dec09f7d197505de86b5ef8c7d8a3
Your Name <you@example.com>
2020-02-21 13:58:46 +0800

    第一次提交

--- app/src/main/java/zhaojianghui20191030/bwie/com/zhaojianghui2020022101/MainActivity.java
+++ app/src/main/java/zhaojianghui20191030/bwie/com/zhaojianghui2020022101/MainActivity.java
@@ -3,6 +3,9 @@ package zhaojianghui20191030.bwie.com.zhaojianghui2020022101;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
